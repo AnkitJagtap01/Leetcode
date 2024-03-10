a7da9231a191b7a684134b90ba7f3874b556103d
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-10 13:34:05 -0400

    Create file

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,35 @@
+
+//·····public·static·void·main(String[]·args)·{
+//·········Solution·solution·=·new·Solution();
+//·········System.out.println(solution.isValid("()"));·//·true
+//·········System.out.println(solution.isValid("()[]{}"));·//·true
+//·········System.out.println(solution.isValid("(]"));·//·false
+//·····}
+//·}
+
+
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
+
+"()"
