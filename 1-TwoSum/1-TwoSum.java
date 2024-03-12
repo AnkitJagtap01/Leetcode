81c2563cbd3d91d2c43850a21dba290a142843e7
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-11 23:46:05 -0400

    Create file

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+// //   time: O(n^2)      
+// //   space: O(1)      
+//                 for (int i = 0; i < nums.length; i++) {
+//             for (int j = i + 1; j < nums.length; j++) {
+//                 if (nums[j] == target - nums[i]) {
+//                     return new int[] { i, j };
+//                 }
+//             }
+//         }
+//         // In case there is no solution, we'll just return null
+//         return null;
+        
+// time: O(n)    
+//  space: O(n)       
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++){
+            int cur = nums[i];
+//             cur + x = target
+//             x = target - cur
+            int x = target - cur;
+            if(map.containsKey(x)){
+                return new int[] {map.get(x), i};
+            }
+            map.put(cur,i);
+        }
+        return null;
+        
+    }
+}
+[2,7,11,15]
