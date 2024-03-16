7e9a2824778fd5dc0e7003766bc7921578d72ed1
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-15 23:55:29 -0400

    Create file

---
+++ 2357-MakeArrayZerobySubtractingEqualAmounts/2357-MakeArrayZerobySubtractingEqualAmounts.java
@@ -0,0 +1,18 @@
+            if (num != 0) {
+                uniqueValues.add(num);
+            }
+        }
+        
+        // The number of operations is the number of unique non-zero values
+        return uniqueValues.size();
+    }
+
+        for (int num : nums) {
+        
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public int minimumOperations(int[] nums) {
+        Set<Integer> uniqueValues = new HashSet<>();
+[
