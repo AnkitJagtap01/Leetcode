6ebad0f9118263be2a6efb7b80456bcd7d91fb5b
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-16 23:39:54 -0400

    Update 2357-MakeArrayZerobySubtractingEqualAmounts.java

--- 2357-MakeArrayZerobySubtractingEqualAmounts/2357-MakeArrayZerobySubtractingEqualAmounts.java
+++ 2357-MakeArrayZerobySubtractingEqualAmounts/2357-MakeArrayZerobySubtractingEqualAmounts.java
@@ -1,18 +1,23 @@
-            if (num != 0) {
-                uniqueValues.add(num);
-            }
-        }
-        
-        // The number of operations is the number of unique non-zero values
-        return uniqueValues.size();
-    }
+import java.util.HashSet;
+import java.util.Set;
 
-        for (int num : nums) {
-        
-import java.util.HashSet;
-import java.util.Set;
+public class Solution {
+    public int minimumOperations(int[] nums) {
+        Set<Integer> uniqueValues = new HashSet<>();
+        
+        for (int num : nums) {
+            if (num != 0) {
+                uniqueValues.add(num);
+            }
+        }
+        
+        // The number of operations is the number of unique non-zero values
+        return uniqueValues.size();
+    }
 
-public class Solution {
-    public int minimumOperations(int[] nums) {
-        Set<Integer> uniqueValues = new HashSet<>();
-[
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.minimumOperations(new int[]{1, 5, 0, 3, 5})); // Output: 3
+        System.out.println(solution.minimumOperations(new int[]{0})); // Output: 0
+    }
+}
