4c4f79165be182a93c3a1908bceaf571413d28b2
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-16 23:56:54 -0400

    Create file

---
+++ 1768-MergeStringsAlternately/1768-MergeStringsAlternately.java
@@ -0,0 +1,18 @@
+            }
+            if (j < word2.length()) {
+                merged.append(word2.charAt(j++));
+            }
+        }
+        return merged.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+                merged.append(word1.charAt(i++));
+        while (i < word1.length() || j < word2.length()) {
+            if (i < word1.length()) {
+public class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder merged = new StringBuilder();
+        int i = 0, j = 0;
+"
