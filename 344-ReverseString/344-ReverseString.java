8afb77b484d834a5d1394061ca0b23c38aebee0e
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-17 21:18:13 -0400

    Update 344-ReverseString.java

--- 344-ReverseString/344-ReverseString.java
+++ 344-ReverseString/344-ReverseString.java
@@ -1,18 +1,17 @@
-class Solution {
-    public void reverseString(char[] s) {
-        
-        int left = 0;
-        int right = s.length-1;
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
 
-        while(left < right){
-            
-    }
-            s[right] = temp;
-            char temp = s[left];
-        }
-            s[left] = s[right];
+        while(left < right){
+            
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
 
-            left++;
-            right--;
+            left++;
+            right--;
+        }
+        
+    }
 }
-[
