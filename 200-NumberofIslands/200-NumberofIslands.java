e8d39f693423b4c68ea8ccc112fef1b83e856cc7
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-23 21:35:49 -0400

    Update 200-NumberofIslands.java

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,18 +1,39 @@
-// Time and Space: O(M * N)
+// Time and Space: O(M * N)
 
-public class Solution {
-    private void dfs(char[][] grid, int r, int c) {
-        int nr = grid.length;
-        int nc = grid[0].length;
+public class Solution {
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
 
-        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
-            return;
-        }
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
 
-        grid[r][c] = '0'; // Mark as visited by sinking the island
-        dfs(grid, r - 1, c); // Up
-        dfs(grid, r + 1, c); // Down
-        dfs(grid, r, c - 1); // Left
-        dfs(grid, r, c + 1); // Right
-    }
-[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0",
+        grid[r][c] = '0'; // Mark as visited by sinking the island
+        dfs(grid, r - 1, c); // Up
+        dfs(grid, r + 1, c); // Down
+        dfs(grid, r, c - 1); // Left
+        dfs(grid, r, c + 1); // Right
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int numIslands = 0;
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++numIslands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        return numIslands;
+    }
+}
