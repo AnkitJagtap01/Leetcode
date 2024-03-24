f6389d1646cf6dd7d94cd6c6eada8ddab420f4bf
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-23 23:59:53 -0400

    Update 1136-ParallelCourses.java

--- 1136-ParallelCourses/1136-ParallelCourses.java
+++ 1136-ParallelCourses/1136-ParallelCourses.java
@@ -1,17 +1,36 @@
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                int course = queue.poll();
-                count++;
-                for (int next : graph.get(course)) {
-                    inDegree[next]--;
-                    if (inDegree[next] == 0) queue.offer(next);
-                }
-            }
-            semester++;
-        }
-        
-        return count == n ? semester : -1;
-    }
-}
+import java.util.*;
 
-3
+public class Solution {
+    public int minimumSemesters(int n, int[][] relations) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] inDegree = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] relation : relations) {
+            graph.get(relation[0]).add(relation[1]);
+            inDegree[relation[1]]++;
+        }
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            if (inDegree[i] == 0) queue.offer(i);
+        }
+        
+        int semester = 0, count = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int course = queue.poll();
+                count++;
+                for (int next : graph.get(course)) {
+                    inDegree[next]--;
+                    if (inDegree[next] == 0) queue.offer(next);
+                }
+            }
+            semester++;
+        }
+        
+        return count == n ? semester : -1;
+    }
+}
