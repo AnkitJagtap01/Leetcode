62fa402409a2ff81058de3c94fad49c059b9d84d
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-23 21:35:10 -0400

    Create file

---
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -0,0 +1,18 @@
+// Time and Space: O(M * N)
+
+public class Solution {
+    private void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] == '0') {
+            return;
+        }
+
+        grid[r][c] = '0'; // Mark as visited by sinking the island
+        dfs(grid, r - 1, c); // Up
+        dfs(grid, r + 1, c); // Down
+        dfs(grid, r, c - 1); // Left
+        dfs(grid, r, c + 1); // Right
+    }
+[["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0",
