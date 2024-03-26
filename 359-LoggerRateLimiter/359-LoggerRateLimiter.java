a8cdc8d7fe1f7d8c90ff2652b411e7add053c2f2
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-25 23:53:28 -0400

    Update 359-LoggerRateLimiter.java

--- 359-LoggerRateLimiter/359-LoggerRateLimiter.java
+++ 359-LoggerRateLimiter/359-LoggerRateLimiter.java
@@ -1,18 +1,30 @@
-class Logger {
+class Logger {
 
-    private Map<String, Integer> messageTimestamps;
+    private Map<String, Integer> messageTimestamps;
 
-    public Logger() {
-        messageTimestamps = new HashMap<>();
-    }
-    
-    public boolean shouldPrintMessage(int timestamp, String message) {
-        
-        // Check if the message has been seen before and the timestamp is not yet allowed
-        
-        if (messageTimestamps.containsKey(message)) {
-            int nextAllowedTimestamp = messageTimestamps.get(message);
-            if (timestamp < nextAllowedTimestamp) {
-                return false; // Message seen before and timestamp not allowed
-            }
-["Logger","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage","shouldPrintMessage",
+    public Logger() {
+        messageTimestamps = new HashMap<>();
+    }
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        
+        // Check if the message has been seen before and the timestamp is not yet allowed
+        
+        if (messageTimestamps.containsKey(message)) {
+            int nextAllowedTimestamp = messageTimestamps.get(message);
+            if (timestamp < nextAllowedTimestamp) {
+                return false; // Message seen before and timestamp not allowed
+            }
+        }
+        
+        // Update the next allowed timestamp for this message
+        messageTimestamps.put(message, timestamp + 10);
+        return true; // Message is either new or allowed to be printed again
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
