ef082647383b1785c82b23a17a266b96059e2d0a
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-25 23:53:55 -0400

    Update 787-CheapestFlightsWithinKStops.java

--- 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.java
+++ 787-CheapestFlightsWithinKStops/787-CheapestFlightsWithinKStops.java
@@ -1,18 +1,24 @@
-// Time: O(K * E) Space: O(N)
-    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[] costs = new int[n];
-        Arrays.fill(costs, Integer.MAX_VALUE);
-        costs[src] = 0;
-        
-        // Perform k + 1 iterations
-        for (int i = 0; i <= k; i++) {
-            int[] temp = costs.clone();
-            for (int[] flight : flights) {
-                int from = flight[0], to = flight[1], price = flight[2];
-                if (costs[from] != Integer.MAX_VALUE && costs[from] + price < temp[to]) {
-                    temp[to] = costs[from] + price;
-                }
-            }
+// Bellman Ford Algorithm
+// Time: O(K * E) Space: O(N)
 
-public class Solution {
-4
+public class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] costs = new int[n];
+        Arrays.fill(costs, Integer.MAX_VALUE);
+        costs[src] = 0;
+        
+        // Perform k + 1 iterations
+        for (int i = 0; i <= k; i++) {
+            int[] temp = costs.clone();
+            for (int[] flight : flights) {
+                int from = flight[0], to = flight[1], price = flight[2];
+                if (costs[from] != Integer.MAX_VALUE && costs[from] + price < temp[to]) {
+                    temp[to] = costs[from] + price;
+                }
+            }
+            costs = temp;
+        }
+        
+        return costs[dst] == Integer.MAX_VALUE ? -1 : costs[dst];
+    }
+}
