82e0b59f25875189047c5a268e3abf36ea72418f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-27 02:45:49 -0400

    Create file

---
+++ 2437-NumberofValidClockTimes/2437-NumberofValidClockTimes.java
@@ -0,0 +1,18 @@
+                ans *= 4;
+                ans *= 10;
+            }
+            else if(str[0] == '2') {
+            ans *= 6;
+        }
+        if(str[1] == '?') {
+            if(str[0] >= '0' && str[0] <= '1') {
+        char[] str = time.toCharArray();
+        int h0 = str[0] - '0', h1 = str[1] - '0';
+        if(str[4] == '?') {
+            ans *= 10;
+        }
+        if(str[3] == '?') {
+class Solution {
+    public int countTime(String time) {
+        int ans = 1;
+"
