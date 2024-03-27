04a125c98631d5d16241b7368ba48ca93900f11d
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-03-26 22:08:58 -0400

    Create file

---
+++ 1110-DeleteNodesAndReturnForest/1110-DeleteNodesAndReturnForest.java
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private Set<Integer> toDeleteSet;
+[1,2,3,4,5,6,7]
