fd44b3bca1564bb1671f6c77555b9d0fa12590f5
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-12 00:07:22 -0400

    Create file

---
+++ 146-LRUCache/146-LRUCache.java
@@ -0,0 +1,19 @@
+    private final int capacity;
+    private int count;
+    private final HashMap<Integer, Node> map;
+    private final Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        head.next = tail;
+        tail.prev = head;
+        this.count = 0;
+    }
+
+import java.util.HashMap;
+
+class LRUCache {
+[
