8240ca663acf1acf55e2cd67b936657e8f18cd85
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-24 08:57:47 -0500

    Update 283-MoveZeroes.java

--- 283-MoveZeroes/283-MoveZeroes.java
+++ 283-MoveZeroes/283-MoveZeroes.java
@@ -1,17 +1,15 @@
-        int insertPos = 0;
-        // Move all non-zero values to the front
-        for (int num : nums) {
-            if (num != 0) {
-                nums[insertPos++] = num;
-            }
-        }
-        // Fill remaining positions with zeros
-        while (insertPos < nums.length) {
-            nums[insertPos++] = 0;
-        }
-    }
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+        // Move all non-zero values to the front
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+        // Fill remaining positions with zeros
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
 }
-
-    public void moveZeroes(int[] nums) {
-public class Solution {
-[
