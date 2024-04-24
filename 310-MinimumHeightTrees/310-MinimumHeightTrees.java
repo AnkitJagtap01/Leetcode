847b653ae2c4f2c9722fe93754c39b2a0d02a616
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-23 22:16:46 -0400

    Create file

---
+++ 310-MinimumHeightTrees/310-MinimumHeightTrees.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) return Collections.singletonList(0);
+
+        List<Set<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new HashSet<>());
+        }
+        
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        
+        // Initialize the first layer of leaves
+4
