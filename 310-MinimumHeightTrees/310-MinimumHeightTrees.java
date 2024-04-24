b7b4de95d411dea3f602a5bee80c94f9ea532b55
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-23 21:49:02 -0500

    Update 310-MinimumHeightTrees.java

--- 310-MinimumHeightTrees/310-MinimumHeightTrees.java
+++ 310-MinimumHeightTrees/310-MinimumHeightTrees.java
@@ -1,17 +1,43 @@
-            }
-            leaves = newLeaves;
-        }
-        
-        return leaves;
-    }
-}
+import java.util.*;
 
-                }
-                    newLeaves.add(neighbor);
-                int neighbor = graph.get(leaf).iterator().next();
-                // remove the leaf from its neighbor's set
-                graph.get(neighbor).remove(leaf);
-                if (graph.get(neighbor).size() == 1) {
-                // the only neighbor
-            for (int leaf : leaves) {
-4
+public class Solution {
+    public List<Integer> findMinHeightTrees(int n, int[][] edges) {
+        if (n == 1) return Collections.singletonList(0);
+
+        List<Set<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new HashSet<>());
+        }
+        
+        for (int[] edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            graph.get(a).add(b);
+            graph.get(b).add(a);
+        }
+        
+        // Initialize the first layer of leaves
+        List<Integer> leaves = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (graph.get(i).size() == 1) leaves.add(i);
+        }
+        
+        // Trim the leaves until reaching the centroids
+        while (n > 2) {
+            n -= leaves.size();
+            List<Integer> newLeaves = new ArrayList<>();
+            for (int leaf : leaves) {
+                // the only neighbor
+                int neighbor = graph.get(leaf).iterator().next();
+                // remove the leaf from its neighbor's set
+                graph.get(neighbor).remove(leaf);
+                if (graph.get(neighbor).size() == 1) {
+                    newLeaves.add(neighbor);
+                }
+            }
+            leaves = newLeaves;
+        }
+        
+        return leaves;
+    }
+}
