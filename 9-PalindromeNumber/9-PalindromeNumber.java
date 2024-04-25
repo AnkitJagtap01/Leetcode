5ac2e211f91487c326e555c3b8975a7f2e754306
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-24 22:51:35 -0500

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,20 @@
+        while (x > reversedHalf) {
+            int lastDigit = x % 10;
+            reversedHalf = reversedHalf * 10 + lastDigit;
+            x /= 10;
+        }
+
+        // The number is a palindrome if the original number is equal to the reversed number
+        // or if the original number is equal to the reversed number without the middle digit (for odd number of digits)
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+        int reversedHalf = 0;
+
+        }
+            return false;
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+    public boolean isPalindrome(int x) {
+        // Negative numbers are not palindromes
+        // Also, if the last digit of the number is 0 and it's not 0 itself, it cannot be a palindrome
+public class Solution {
+1
