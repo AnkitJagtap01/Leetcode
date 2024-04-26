4c94d202f55c3260691cd633a3d2ef39da11e7ff
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-25 23:45:29 -0500

    Update 412-FizzBuzz.java

--- 412-FizzBuzz/412-FizzBuzz.java
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -1,17 +1,26 @@
-            } else if (divisibleBy5) {
-                // Divisible by 5, add "Buzz"
-                result.add("Buzz");
-            } else {
-                // Not divisible by 3 or 5, add the number itself
-                result.add(Integer.toString(i));
-            }
-        }
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
 
-        return result;
-    }
+        for (int i = 1; i <= n; i++) {
+            boolean divisibleBy3 = (i % 3 == 0);
+            boolean divisibleBy5 = (i % 5 == 0);
+
+            if (divisibleBy3 && divisibleBy5) {
+                // Divisible by both 3 and 5, add "FizzBuzz"
+                result.add("FizzBuzz");
+            } else if (divisibleBy3) {
+                // Divisible by 3, add "Fizz"
+                result.add("Fizz");
+            } else if (divisibleBy5) {
+                // Divisible by 5, add "Buzz"
+                result.add("Buzz");
+            } else {
+                // Not divisible by 3 or 5, add the number itself
+                result.add(Integer.toString(i));
+            }
+        }
+
+        return result;
+    }
 }
-
-                result.add("Fizz");
-                // Divisible by 3, add "Fizz"
-            } else if (divisibleBy3) {
-3
