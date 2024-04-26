a9a35df65e3641888d90ec4c666b07d7a8ab48e6
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-26 11:56:45 -0500

    Update 6-ZigzagConversion.java

--- 6-ZigzagConversion/6-ZigzagConversion.java
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -1,20 +1,30 @@
-        boolean goingDown = false;
+public class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1 || s.length() <= numRows) {
+            return s;
+        }
 
-        for (char c : s.toCharArray()) {
-            rows[currentRow].append(c);
-            if (currentRow == 0 || currentRow == numRows - 1) {
-                goingDown = !goingDown;
-            }
-            currentRow += goingDown ? 1 : -1;
-        }
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new StringBuilder();
+        }
 
-        StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
-            result.append(row);
-        }
+        int currentRow = 0;
+        boolean goingDown = false;
 
-        return result.toString();
-    }
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
 }
-
-"
