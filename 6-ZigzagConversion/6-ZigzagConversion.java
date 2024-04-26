fc5009cf07d79bb26f9bf63cb3d581d668bb21a6
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-26 11:56:23 -0500

    Create file

---
+++ 6-ZigzagConversion/6-ZigzagConversion.java
@@ -0,0 +1,20 @@
+        boolean goingDown = false;
+
+        for (char c : s.toCharArray()) {
+            rows[currentRow].append(c);
+            if (currentRow == 0 || currentRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currentRow += goingDown ? 1 : -1;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder row : rows) {
+            result.append(row);
+        }
+
+        return result.toString();
+    }
+}
+
+"
