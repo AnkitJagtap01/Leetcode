68b7c2555abe62ec0dff1cc9eaa797501e1b9762
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-25 23:34:51 -0500

    Update 1913-MaximumProductDifferenceBetweenTwoPairs.java

--- 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.java
+++ 1913-MaximumProductDifferenceBetweenTwoPairs/1913-MaximumProductDifferenceBetweenTwoPairs.java
@@ -1,12 +1,10 @@
-class Solution {
-    public int maxProductDifference(int[] nums) {
-    Arrays.sort(nums);
-    int n = nums.length;
-    // Two largest numbers: nums[n-1], nums[n-2]
-    // Two smallest numbers: nums[0], nums[1]
-    int productDifference = (nums[n-1] * nums[n-2]) - (nums[0] * nums[1]);
-    return productDifference;
+class Solution {
+    public int maxProductDifference(int[] nums) {
+    Arrays.sort(nums);
+    int n = nums.length;
+    // Two largest numbers: nums[n-1], nums[n-2]
+    // Two smallest numbers: nums[0], nums[1]
+    int productDifference = (nums[n-1] * nums[n-2]) - (nums[0] * nums[1]);
+    return productDifference;
 }
 }
-
-[
