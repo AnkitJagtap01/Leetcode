85fa0195b18856b2645d0e6b09e7257408aa4bef
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-25 23:44:54 -0500

    Create file

---
+++ 412-FizzBuzz/412-FizzBuzz.java
@@ -0,0 +1,17 @@
+            } else if (divisibleBy5) {
+                // Divisible by 5, add "Buzz"
+                result.add("Buzz");
+            } else {
+                // Not divisible by 3 or 5, add the number itself
+                result.add(Integer.toString(i));
+            }
+        }
+
+        return result;
+    }
+}
+
+                result.add("Fizz");
+                // Divisible by 3, add "Fizz"
+            } else if (divisibleBy3) {
+3
