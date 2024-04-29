d26af8e94a89b2b786fb1209cd60d52f0ea73396
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-29 00:00:52 -0500

    Update 35-SearchInsertPosition.java

--- 35-SearchInsertPosition/35-SearchInsertPosition.java
+++ 35-SearchInsertPosition/35-SearchInsertPosition.java
@@ -1,20 +1,19 @@
-    public int searchInsert(int[] nums, int target) {
-        int low = 0;
-        int high = nums.length - 1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
-            }
-        }
-        
-        return low; // If target is not found, low will be at the correct insertion index.
-    }
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        
+        return low; // If target is not found, low will be at the correct insertion index.
+    }
 }
-
-[
