937db915a5e16ffde55150851a294fd95dd56c99
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-29 00:02:54 -0500

    Update 2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK.java

--- 2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK/2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK.java
+++ 2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK/2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK.java
@@ -1,20 +1,26 @@
-            if ((k % 2) != (finalXor % 2)) {
-                count++;
-            }
-            
-            // Remove the last bit from both integers.
-            // k % 2 returns the rightmost bit in k,
-            // finalXor % 2 returns the rightmost bit in finalXor.
-            // Increment counter, if the bits don't match.
-        while (k > 0 || finalXor > 0) {
-        // Keep iterating until any of k or finalXor becomes zero.
-        int count = 0;
-        
-            finalXor = finalXor ^ n;
-        }
-        for (int n : nums) {
-        // XOR of all integers in the array.
-        int finalXor = 0;
-    public int minOperations(int[] nums, int k) {
-class Solution {
-[
+class Solution {
+    public int minOperations(int[] nums, int k) {
+        int finalXor = 0;
+        // XOR of all integers in the array.
+        for (int n : nums) {
+            finalXor = finalXor ^ n;
+        }
+        
+        int count = 0;
+        // Keep iterating until any of k or finalXor becomes zero.
+        while (k > 0 || finalXor > 0) {
+            // k % 2 returns the rightmost bit in k,
+            // finalXor % 2 returns the rightmost bit in finalXor.
+            // Increment counter, if the bits don't match.
+            if ((k % 2) != (finalXor % 2)) {
+                count++;
+            }
+            
+            // Remove the last bit from both integers.
+            k /= 2;
+            finalXor /= 2;
+        }
+        
+        return count;
+    }
+}
