111cfb592efb955857ab458eb20835823d7f421f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-28 23:58:22 -0500

    Update 27-RemoveElement.java

--- 27-RemoveElement/27-RemoveElement.java
+++ 27-RemoveElement/27-RemoveElement.java
@@ -1,14 +1,12 @@
-public class Solution {
-    public int removeElement(int[] nums, int val) {
-        int i = 0; // Pointer for placing the next non-val element
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != val) {
-                nums[i] = nums[j];
-                i++;
-            }
-        }
-        return i; // Return the number of elements not equal to val
-    }
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0; // Pointer for placing the next non-val element
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i; // Return the number of elements not equal to val
+    }
 }
-
-[
