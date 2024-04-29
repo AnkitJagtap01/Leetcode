2aaa5972c06fa8e2d492d7f0c6c3918ca6dde477
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-29 00:02:17 -0500

    Create file

---
+++ 2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK/2997-MinimumNumberofOperationstoMakeArrayXOREqualtoK.java
@@ -0,0 +1,20 @@
+            if ((k % 2) != (finalXor % 2)) {
+                count++;
+            }
+            
+            // Remove the last bit from both integers.
+            // k % 2 returns the rightmost bit in k,
+            // finalXor % 2 returns the rightmost bit in finalXor.
+            // Increment counter, if the bits don't match.
+        while (k > 0 || finalXor > 0) {
+        // Keep iterating until any of k or finalXor becomes zero.
+        int count = 0;
+        
+            finalXor = finalXor ^ n;
+        }
+        for (int n : nums) {
+        // XOR of all integers in the array.
+        int finalXor = 0;
+    public int minOperations(int[] nums, int k) {
+class Solution {
+[
