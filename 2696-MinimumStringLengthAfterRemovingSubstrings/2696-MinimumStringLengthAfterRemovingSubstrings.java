2b5f0c695b05e61218cf9ceacda790272e1885a8
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-29 23:24:32 -0500

    Update 2696-MinimumStringLengthAfterRemovingSubstrings.java

--- 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.java
+++ 2696-MinimumStringLengthAfterRemovingSubstrings/2696-MinimumStringLengthAfterRemovingSubstrings.java
@@ -1,16 +1,15 @@
-class Solution {
-    public int minLength(String s) {
-        Stack<Character> stack = new Stack<>();
+class Solution {
+    public int minLength(String s) {
+        Stack<Character> stack = new Stack<>();
 
-        for (char ch : s.toCharArray()) {
-            if (!stack.isEmpty() && ((ch == 'B' && stack.peek() == 'A') || (ch == 'D' && stack.peek() == 'C'))) {
-                stack.pop(); // Remove the top of the stack if it forms "AB" or "CD" with ch
-            } else {
-                stack.push(ch); // Push the current character onto the stack
-            }
-        }
+        for (char ch : s.toCharArray()) {
+            if (!stack.isEmpty() && ((ch == 'B' && stack.peek() == 'A') || (ch == 'D' && stack.peek() == 'C'))) {
+                stack.pop(); // Remove the top of the stack if it forms "AB" or "CD" with ch
+            } else {
+                stack.push(ch); // Push the current character onto the stack
+            }
+        }
 
-        return stack.size(); // The size of the stack is the minimum length of the resulting string
-    }
+        return stack.size(); // The size of the stack is the minimum length of the resulting string
+    }
 }
-"
