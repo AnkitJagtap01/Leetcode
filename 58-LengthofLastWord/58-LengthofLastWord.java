30bf7aaa4091d082023e74cf75925fa92daa8f8d
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-04-30 22:37:00 -0500

    Create file

---
+++ 58-LengthofLastWord/58-LengthofLastWord.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int length = 0;
+        boolean charFlag = false;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (Character.isLetter(s.charAt(i))) {
+                // Start counting the length of the last word
+                charFlag = true;
+                length++;
+            } else {
+                // If we have started counting and encounter a space, stop.
+                if (charFlag) {
+                    return length;
+                }
+            }
+        }
+        
+        return length;
+    }
+}
+
+"
