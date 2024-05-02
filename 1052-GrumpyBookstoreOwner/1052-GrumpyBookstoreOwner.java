855ded5bd6009a13197ab1c7177cad233cb24259
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-01 23:53:47 -0500

    Create file

---
+++ 1052-GrumpyBookstoreOwner/1052-GrumpyBookstoreOwner.java
@@ -0,0 +1,20 @@
+                extraSatisfied += customers[i];
+            if (grumpy[i] == 1) {
+            // If the owner is grumpy at this minute, consider satisfying these customers using the technique
+        for (int i = 0; i < n; i++) {
+        // Using a sliding window to find the best interval to apply the technique
+
+        }
+            }
+                totalSatisfied += customers[i];
+            if (grumpy[i] == 0) {
+        for (int i = 0; i < n; i++) {
+        // Calculate base satisfied customers without using the technique
+
+        int extraSatisfied = 0;
+        int maxExtraSatisfied = 0;
+        int totalSatisfied = 0;
+        int n = customers.length;
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+public class Solution {
+[
