fd7f0e169f449120e9be8b48be8fb0426ebce1c3
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-01 23:51:54 -0500

    Update 70-ClimbingStairs.java

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,20 +1,19 @@
-class Solution {
-    public int climbStairs(int n) {
-        if (n == 1) {
-            return 1;
-        }
+class Solution {
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
 
-        // Base cases
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
+        // Base cases
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
 
-        // Fill the dp array
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
+        // Fill the dp array
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
-        return dp[n];
-    }
+        return dp[n];
+    }
 }
-2
