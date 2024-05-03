2174f50e555e38fe6e15aaec48eb5f8c9a86ec69
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-02 22:45:27 -0500

    Update 841-KeysandRooms.java

--- 841-KeysandRooms/841-KeysandRooms.java
+++ 841-KeysandRooms/841-KeysandRooms.java
@@ -1,26 +1,22 @@
+public class Solution {
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        boolean[] visited = new boolean[rooms.size()];
+        int[] visitCount = {0}; // Using array to mutate the value inside the DFS method
 
-        // If visitCount matches the number of rooms, we've visited all rooms
-        return visitCount[0] == rooms.size();
-    }
+        // Start DFS from room 0
+        dfs(rooms, 0, visited, visitCount);
 
-    private void dfs(List<List<Integer>> rooms, int room, boolean[] visited, int[] visitCount) {
-        visited[room] = true;
-        visitCount[0]++;
-        for (int key : rooms.get(room)) {
-            if (!visited[key]) {
-                dfs(rooms, key, visited, visitCount);
-            }
-        }
-    }
+        // If visitCount matches the number of rooms, we've visited all rooms
+        return visitCount[0] == rooms.size();
+    }
+
+    private void dfs(List<List<Integer>> rooms, int room, boolean[] visited, int[] visitCount) {
+        visited[room] = true;
+        visitCount[0]++;
+        for (int key : rooms.get(room)) {
+            if (!visited[key]) {
+                dfs(rooms, key, visited, visitCount);
+            }
+        }
+    }
 }
-
-        int[] visitCount = {0}; // Using array to mutate the value inside the DFS method
-
-        // Start DFS from room 0
-        dfs(rooms, 0, visited, visitCount);
-        boolean[] visited = new boolean[rooms.size()];
-public class Solution {
-    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
-import java.util.*;
-
-[
