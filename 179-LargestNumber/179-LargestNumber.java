69dcdfffd438e09dc4fee0e2f47c93f36bf3b0b5
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-02 22:52:02 -0500

    Update 179-LargestNumber.java

--- 179-LargestNumber/179-LargestNumber.java
+++ 179-LargestNumber/179-LargestNumber.java
@@ -1,26 +1,31 @@
-        
-        // If the largest number is "0", the whole number is zero (edge case)
-        if (asStrs[0].equals("0")) {
-            return "0";
-        }
-        
-        // Build the largest number from sorted components
-                String order1 = a + b;
-                String order2 = b + a;
-                return order2.compareTo(order1);
-            }
-        });
-            public int compare(String a, String b) {
-        Arrays.sort(asStrs, new Comparator<String>() {
-        }
-        
-        // Sort strings according to custom comparator
-    public String largestNumber(int[] nums) {
-        // Convert integers to strings for comparison
-        String[] asStrs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            asStrs[i] = String.valueOf(nums[i]);
-public class Solution {
-        StringBuilder largestNumberStr = new StringBuilder();
-        for (String numAsStr : asStrs) {
-[
+public class Solution {
+    public String largestNumber(int[] nums) {
+        // Convert integers to strings for comparison
+        String[] asStrs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            asStrs[i] = String.valueOf(nums[i]);
+        }
+        
+        // Sort strings according to custom comparator
+        Arrays.sort(asStrs, new Comparator<String>() {
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1);
+            }
+        });
+        
+        // If the largest number is "0", the whole number is zero (edge case)
+        if (asStrs[0].equals("0")) {
+            return "0";
+        }
+        
+        // Build the largest number from sorted components
+        StringBuilder largestNumberStr = new StringBuilder();
+        for (String numAsStr : asStrs) {
+            largestNumberStr.append(numAsStr);
+        }
+        
+        return largestNumberStr.toString();
+    }
+}
