3ee50411c7cbeb70ed4c8c0d8e957a9fa196518c
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-03 23:51:36 -0500

    Update 881-BoatstoSavePeople.java

--- 881-BoatstoSavePeople/881-BoatstoSavePeople.java
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -1,19 +1,17 @@
+public class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int boats = 0;
+
+        while (i <= j) {
+            if (people[i] + people[j] <= limit) {
+                i++;  // person i can pair with person j, so move the lighter person pointer
+            }
+            j--;  // always move the heavier person pointer
+            boats++;  // we used one boat, whether it's a pair or a single person
+        }
+
+        return boats;
+    }
 }
-    }
-        return boats;
-
-        int boats = 0;
-
-        while (i <= j) {
-            if (people[i] + people[j] <= limit) {
-                i++;  // person i can pair with person j, so move the lighter person pointer
-            }
-            j--;  // always move the heavier person pointer
-            boats++;  // we used one boat, whether it's a pair or a single person
-        }
-        int i = 0, j = people.length - 1;
-        Arrays.sort(people);
-    public int numRescueBoats(int[] people, int limit) {
-public class Solution {
-
-[
