70210060043aadb958edf35fcf7240b65a9e727a
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-03 23:56:11 -0500

    Update 680-ValidPalindromeII.java

--- 680-ValidPalindromeII/680-ValidPalindromeII.java
+++ 680-ValidPalindromeII/680-ValidPalindromeII.java
@@ -1,20 +1,34 @@
-public class Solution {
-    public boolean validPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                // Check two possible palindromes: one skipping the left character, one skipping the right character
-                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
-            }
-            left++;
-            right--;
-        }
-        
-        return true; // No need to delete any character
-    }
-    
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-"aba"
+public class Solution {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                // Check two possible palindromes: one skipping the left character, one skipping the right character
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        
+        return true; // No need to delete any character
+    }
+    
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.validPalindrome("aba"));  // Output: true
+        System.out.println(solution.validPalindrome("abca")); // Output: true
+        System.out.println(solution.validPalindrome("abc"));  // Output: false
+    }
+}
