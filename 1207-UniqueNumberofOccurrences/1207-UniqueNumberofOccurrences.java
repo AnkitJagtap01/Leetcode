a1df40226affbd5f6b03a95cb9c200c3869a9bfd
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-04 23:36:08 -0500

    Update 1207-UniqueNumberofOccurrences.java

--- 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.java
+++ 1207-UniqueNumberofOccurrences/1207-UniqueNumberofOccurrences.java
@@ -1,20 +1,21 @@
-            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
-        }
-        
-        // Check if all frequencies are unique
-        for (int count : frequencyMap.values()) {
-            if (!occurrences.add(count)) {
-                // If add returns false, the count was already in the set, thus it's not unique
-                return false;
-            }
-        }
-        
-        return true;
-        for (int num : arr) {
-        // Count the frequency of each element
-        Set<Integer> occurrences = new HashSet<>();
-        
-    public boolean uniqueOccurrences(int[] arr) {
-        Map<Integer, Integer> frequencyMap = new HashMap<>();
-class Solution {
-[
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> frequencyMap = new HashMap<>();
+        Set<Integer> occurrences = new HashSet<>();
+        
+        // Count the frequency of each element
+        for (int num : arr) {
+            frequencyMap.put(num, frequencyMap.getOrDefault(num, 0) + 1);
+        }
+        
+        // Check if all frequencies are unique
+        for (int count : frequencyMap.values()) {
+            if (!occurrences.add(count)) {
+                // If add returns false, the count was already in the set, thus it's not unique
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
