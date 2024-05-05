7e6b6dbb530dd90c6a219f04d6c7d0692984fb21
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-04 23:25:16 -0500

    Update 237-DeleteNodeinaLinkedList.java

--- 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
+++ 237-DeleteNodeinaLinkedList/237-DeleteNodeinaLinkedList.java
@@ -1,20 +1,24 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
 
-public class Solution {
-    public void deleteNode(ListNode node) {
-        node.val = node.next.val; // Copy the value from the next node to the current node.
-        node.next = node.next.next; // Unlink the next node from the list.
-    }
-    
-    private static void printList(ListNode head) {
-        ListNode current = head;
-        while (current != null) {
-            System.out.print(current.val + " -> ");
-[
+public class Solution {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val; // Copy the value from the next node to the current node.
+        node.next = node.next.next; // Unlink the next node from the list.
+    }
+    
+    private static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+}
