d28e20c5571f0898971961960762e8d85bd89f55
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-04 23:28:49 -0500

    Create file

---
+++ 1512-NumberofGoodPairs/1512-NumberofGoodPairs.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int goodPairs = 0;
+        Map<Integer, Integer> count = new HashMap<>();  // HashMap to store frequency of each element
+        
+        for (int num : nums) {
+            if (count.containsKey(num)) {
+                // If num already exists in map, increment goodPairs by the number of times num has been seen before
+                goodPairs += count.get(num);
+            }
+            // Increment the count of num in the map
+            count.put(num, count.getOrDefault(num, 0) + 1);
+        }
+        
+        return goodPairs;
+    }
+}
+
+[
