32bc12c4cc68e10591643942315934ef40e3344b
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-05 23:06:40 -0500

    Update 83-RemoveDuplicatesfromSortedList.java

--- 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.java
+++ 83-RemoveDuplicatesfromSortedList/83-RemoveDuplicatesfromSortedList.java
@@ -1,20 +1,28 @@
-        ListNode current = head;
-    public ListNode deleteDuplicates(ListNode head) {
-        
-        while (current != null && current.next != null) {
-            if (current.val == current.next.val) {
-                // Skip the duplicate
-                current.next = current.next.next;
-class Solution {
- * }
- */
-
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- *     ListNode next;
- *     ListNode() {}
- * public class ListNode {
- *     int val;
 /**
- * Definition for singly-linked list.
-[
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode current = head;
+        
+        while (current != null && current.next != null) {
+            if (current.val == current.next.val) {
+                // Skip the duplicate
+                current.next = current.next.next;
+            } else {
+                // Move to the next distinct element
+                current = current.next;
+            }
+        }
+        
+        return head;
+    }
+}
