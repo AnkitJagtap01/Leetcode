19477b00a6086af0c69463ab6ebf6cef928cbf95
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-05 23:12:31 -0500

    Update 118-Pascal'sTriangle.java

--- 118-Pascal'sTriangle/118-Pascal'sTriangle.java
+++ 118-Pascal'sTriangle/118-Pascal'sTriangle.java
@@ -1,20 +1,30 @@
-class Solution {
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> triangle = new ArrayList<>();
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
 
-        // Base case; first row is always [1].
-        triangle.add(new ArrayList<>());
-        triangle.get(0).add(1);
+        // Base case; first row is always [1].
+        triangle.add(new ArrayList<>());
+        triangle.get(0).add(1);
 
-        for (int rowNum = 1; rowNum < numRows; rowNum++) {
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = triangle.get(rowNum - 1);
+        for (int rowNum = 1; rowNum < numRows; rowNum++) {
+            List<Integer> row = new ArrayList<>();
+            List<Integer> prevRow = triangle.get(rowNum - 1);
 
-            // The first row element is always 1.
-            row.add(1);
+            // The first row element is always 1.
+            row.add(1);
 
-            // Each triangle element (except the first and last of each row)
-            // is equal to the sum of the elements above-and-to-the-left and above-and-to-the-right.
-            for (int j = 1; j < rowNum; j++) {
-                row.add(prevRow.get(j - 1) + prevRow.get(j));
-5
+            // Each triangle element (except the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and above-and-to-the-right.
+            for (int j = 1; j < rowNum; j++) {
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+
+            // The last row element is always 1.
+            row.add(1);
+
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
