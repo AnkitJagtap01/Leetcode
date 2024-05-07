4f5e08bf9e01da0ff667c6552876e6c4d01e1643
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-06 23:20:03 -0500

    Update 117-PopulatingNextRightPointersinEachNodeII.java

--- 117-PopulatingNextRightPointersinEachNodeII/117-PopulatingNextRightPointersinEachNodeII.java
+++ 117-PopulatingNextRightPointersinEachNodeII/117-PopulatingNextRightPointersinEachNodeII.java
@@ -1,20 +1,65 @@
-                    } else {
-                        head = current.right;
-                    }
-                    prev = current.right;
-                }
-                // move to next node
-                current = current.next;
-            }
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
 
-            // move to next level
-            current = head;
-            head = null;
-            prev = null;
-        }
+    public Node() {}
 
-        return root;
-    }
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if (root == null) return null;
+
+        Node head = null; // head of the next level
+        Node prev = null; // the leading node on the next level
+        Node current = root; // current node of current level
+
+        while (current != null) {
+            // iterate on the current level
+            while (current != null) {
+                // left child
+                if (current.left != null) {
+                    if (prev != null) {
+                        prev.next = current.left;
+                    } else {
+                        head = current.left;
+                    }
+                    prev = current.left;
+                }
+                // right child
+                if (current.right != null) {
+                    if (prev != null) {
+                        prev.next = current.right;
+                    } else {
+                        head = current.right;
+                    }
+                    prev = current.right;
+                }
+                // move to next node
+                current = current.next;
+            }
+
+            // move to next level
+            current = head;
+            head = null;
+            prev = null;
+        }
+
+        return root;
+    }
 }
-
-[
