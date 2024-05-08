021d188631a01acbf33be463963ad2e542752c48
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-08 00:00:15 -0500

    Create file

---
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+ */
+ *     }
+ * }
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+/**
+[
