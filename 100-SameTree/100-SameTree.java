df4f3a476abe103f24faef2f59b817144254429a
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-07 23:57:35 -0500

    Create file

---
+++ 100-SameTree/100-SameTree.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // If both nodes are null, trees are the same
+        if (p == null && q == null) return true;
+        
+        // If one of the nodes is null, trees are not the same
+        if (p == null || q == null) return false;
+        
+        // If the values of current nodes do not match
+
+ */
+ * }
+ *     }
+ *         this.right = right;
+ *         this.left = left;
+ *         this.val = val;
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode() {}
+ *     TreeNode right;
+ *     TreeNode left;
+[
