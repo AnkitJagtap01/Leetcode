7d57fd7cae28946aa7a91c3b7f639bf905d00d5f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-07 23:55:04 -0500

    Update 94-BinaryTreeInorderTraversal.java

--- 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.java
@@ -1,21 +1,40 @@
-                stack.push(current);
-                current = current.left;
-            }
-            
-            // Process node
-            current = stack.pop();
-            result.add(current.val);
-            
-            // Move to right subtree
-            current = current.right;
-        }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 
-        return result;
-    }
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            // Push all left children to stack
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            
+            // Process node
+            current = stack.pop();
+            result.add(current.val);
+            
+            // Move to right subtree
+            current = current.right;
+        }
+
+        return result;
+    }
 }
-            while (current != null) {
-            // Push all left children to stack
-        while (current != null || !stack.isEmpty()) {
-
-        TreeNode current = root;
-[
