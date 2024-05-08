fb4ab0510a9b5a4dcdb968c1c48435aa66c6cc4a
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-08 00:00:57 -0500

    Update 101-SymmetricTree.java

--- 101-SymmetricTree/101-SymmetricTree.java
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -1,20 +1,29 @@
-
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
- */
- *     }
- * }
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
 /**
-[
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode left, TreeNode right) {
+        if (left == null && right == null) return true;
+        if (left == null || right == null) return false;
+        if (left.val != right.val) return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+}
