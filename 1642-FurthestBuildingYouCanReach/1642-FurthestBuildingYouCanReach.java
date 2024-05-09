a80d25bcbee3bb6e03031964f958a0b9c866ef3d
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-08 23:45:02 -0500

    Update 1642-FurthestBuildingYouCanReach.java

--- 1642-FurthestBuildingYouCanReach/1642-FurthestBuildingYouCanReach.java
+++ 1642-FurthestBuildingYouCanReach/1642-FurthestBuildingYouCanReach.java
@@ -1,26 +1,32 @@
-        for (int i = 0; i < heights.length - 1; i++) {
-            int diff = heights[i + 1] - heights[i];
-            
-            // Only need to consider when the next building is taller
-            if (diff > 0) {
-                // Use a ladder for this jump
-                ladderAllocations.add(diff);
-                
-                // If we have used more ladders than available
-                if (ladderAllocations.size() > ladders) {
-                    // Replace the smallest ladder usage with bricks
-                    bricks -= ladderAllocations.poll();
-        
-        PriorityQueue<Integer> ladderAllocations = new PriorityQueue<>();
-        // Priority queue to use the largest jumps with ladders
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
-// Time Complexity: O(n log k)
-                }
-                
-                // If we run out of bricks, we can't move to the next building
-                if (bricks < 0) {
-                    return i;
+// Time Complexity: O(n log k)
+// Space Complexity: O(k)
 
-public class Solution {
-// Space Complexity: O(k)
-[
+public class Solution {
+    public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        // Priority queue to use the largest jumps with ladders
+        PriorityQueue<Integer> ladderAllocations = new PriorityQueue<>();
+        
+        for (int i = 0; i < heights.length - 1; i++) {
+            int diff = heights[i + 1] - heights[i];
+            
+            // Only need to consider when the next building is taller
+            if (diff > 0) {
+                // Use a ladder for this jump
+                ladderAllocations.add(diff);
+                
+                // If we have used more ladders than available
+                if (ladderAllocations.size() > ladders) {
+                    // Replace the smallest ladder usage with bricks
+                    bricks -= ladderAllocations.poll();
+                }
+                
+                // If we run out of bricks, we can't move to the next building
+                if (bricks < 0) {
+                    return i;
+                }
+            }
+        }
+        
+        return heights.length - 1;
+    }
+}
