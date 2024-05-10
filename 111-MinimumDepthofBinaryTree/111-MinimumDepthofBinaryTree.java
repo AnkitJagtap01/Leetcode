a9fc7d6f1cdf87a4979b2a061a690a2e7bd11451
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-09 23:55:26 -0500

    Create file

---
+++ 111-MinimumDepthofBinaryTree/111-MinimumDepthofBinaryTree.java
@@ -0,0 +1,26 @@
+        while (!queue.isEmpty()) {
+        queue.offer(root);
+        int depth = 1;  // Start with depth 1 since we are counting from the root node
+        
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+ *     }
+ * }
+ */
+
+public class Solution {
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+[
