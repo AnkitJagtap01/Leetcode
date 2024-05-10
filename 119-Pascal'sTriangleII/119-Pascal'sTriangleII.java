ff6e7940c81f5971c0d607ce3df56a486dd9adb6
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-09 23:47:29 -0500

    Update 119-Pascal'sTriangleII.java

--- 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
+++ 119-Pascal'sTriangleII/119-Pascal'sTriangleII.java
@@ -1,16 +1,14 @@
-public class Solution {
-    public List<Integer> getRow(int rowIndex) {
-        List<Integer> row = new ArrayList<>(Collections.nCopies(rowIndex + 1, 0));
-        row.set(0, 1); // First element is always 1
-        
-        for (int i = 1; i <= rowIndex; i++) {
-            for (int j = i; j > 0; j--) {
-                row.set(j, row.get(j) + row.get(j - 1));
-            }
-        }
-        
-        return row;
-    }
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>(Collections.nCopies(rowIndex + 1, 0));
+        row.set(0, 1); // First element is always 1
+        
+        for (int i = 1; i <= rowIndex; i++) {
+            for (int j = i; j > 0; j--) {
+                row.set(j, row.get(j) + row.get(j - 1));
+            }
+        }
+        
+        return row;
+    }
 }
-
-3
