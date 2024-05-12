fcc54eda68af6fc9a95634672578f0350c589749
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-12 13:12:37 -0500

    Update 2373-LargestLocalValuesinaMatrix.java

--- 2373-LargestLocalValuesinaMatrix/2373-LargestLocalValuesinaMatrix.java
+++ 2373-LargestLocalValuesinaMatrix/2373-LargestLocalValuesinaMatrix.java
@@ -1,20 +1,23 @@
-            // Find the maximum in the 3x3 grid starting at (i, j)
-            int maxVal = 0;
-            for (int x = i; x < i + 3; x++) {
-                for (int y = j; y < j + 3; y++) {
-                    maxVal = Math.max(maxVal, grid[x][y]);
-                }
-            }
-            // Set the maximum value in the result matrix
-            maxLocal[i][j] = maxVal;
-        }
-    }
-    for (int i = 0; i <= n - 3; i++) {
-        for (int j = 0; j <= n - 3; j++) {
-    // Iterate through each cell that can be the top-left corner of a 3x3 matrix
-    int n = grid.length;
-    int[][] maxLocal = new int[n-2][n-2];
-    
-class Solution {
-    public int[][] largestLocal(int[][] grid) {
-[
+class Solution {
+    public int[][] largestLocal(int[][] grid) {
+    int n = grid.length;
+    int[][] maxLocal = new int[n-2][n-2];
+    
+    // Iterate through each cell that can be the top-left corner of a 3x3 matrix
+    for (int i = 0; i <= n - 3; i++) {
+        for (int j = 0; j <= n - 3; j++) {
+            // Find the maximum in the 3x3 grid starting at (i, j)
+            int maxVal = 0;
+            for (int x = i; x < i + 3; x++) {
+                for (int y = j; y < j + 3; y++) {
+                    maxVal = Math.max(maxVal, grid[x][y]);
+                }
+            }
+            // Set the maximum value in the result matrix
+            maxLocal[i][j] = maxVal;
+        }
+    }
+    
+    return maxLocal;
+}
+}
