c1789a980cb219e6788638ba80c17e85d058099f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-12 20:18:25 -0500

    Update 136-SingleNumber.java

--- 136-SingleNumber/136-SingleNumber.java
+++ 136-SingleNumber/136-SingleNumber.java
@@ -1,11 +1,9 @@
-            singleNumber ^= num;
-        }
-        return singleNumber;
-    }
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+        for (int num : nums) {
+            singleNumber ^= num;
+        }
+        return singleNumber;
+    }
 }
-
-class Solution {
-    public int singleNumber(int[] nums) {
-        int singleNumber = 0;
-        for (int num : nums) {
-[
