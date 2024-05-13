dee52e1954eec273669a239fc9f5b5b5507959eb
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-12 20:18:05 -0500

    Create file

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,11 @@
+            singleNumber ^= num;
+        }
+        return singleNumber;
+    }
+}
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+        for (int num : nums) {
+[
