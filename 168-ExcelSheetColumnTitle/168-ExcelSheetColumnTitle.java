f30007d2cc9ea9c33ccb93b59ffea45da02f97c3
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-13 23:50:29 -0500

    Update 168-ExcelSheetColumnTitle.java

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,16 +1,14 @@
-public class Solution {
-    public String convertToTitle(int columnNumber) {
-        StringBuilder columnTitle = new StringBuilder();
-        
-        while (columnNumber > 0) {
-            columnNumber--; // Adjust for 1-based indexing
-            int remainder = columnNumber % 26;
-            columnTitle.append((char) (remainder + 'A'));
-            columnNumber /= 26;
-        }
-        
-        return columnTitle.reverse().toString();
-    }
+public class Solution {
+    public String convertToTitle(int columnNumber) {
+        StringBuilder columnTitle = new StringBuilder();
+        
+        while (columnNumber > 0) {
+            columnNumber--; // Adjust for 1-based indexing
+            int remainder = columnNumber % 26;
+            columnTitle.append((char) (remainder + 'A'));
+            columnNumber /= 26;
+        }
+        
+        return columnTitle.reverse().toString();
+    }
 }
-
-1
