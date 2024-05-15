22244ea2a4ea6a36c43ab737ad97051dd68ea19b
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-14 23:18:14 -0500

    Update 170-TwoSumIII-Datastructuredesign.java

--- 170-TwoSumIII-Datastructuredesign/170-TwoSumIII-Datastructuredesign.java
+++ 170-TwoSumIII-Datastructuredesign/170-TwoSumIII-Datastructuredesign.java
@@ -1,20 +1,38 @@
-public class TwoSum {
-    private Map<Integer, Integer> numCounts;
+public class TwoSum {
+    private Map<Integer, Integer> numCounts;
 
-    /** Initialize data structure. */
-    public TwoSum() {
-        numCounts = new HashMap<>();
-    }
+    /** Initialize data structure. */
+    public TwoSum() {
+        numCounts = new HashMap<>();
+    }
 
-    /** Add the number to an internal data structure. */
-    public void add(int number) {
-        numCounts.put(number, numCounts.getOrDefault(number, 0) + 1);
-    }
+    /** Add the number to an internal data structure. */
+    public void add(int number) {
+        numCounts.put(number, numCounts.getOrDefault(number, 0) + 1);
+    }
 
-    /** Find if there exists any pair of numbers which sum is equal to the value. */
-    public boolean find(int value) {
-        for (Map.Entry<Integer, Integer> entry : numCounts.entrySet()) {
-            int num = entry.getKey();
-            int complement = value - num;
-            if (complement != num) {
-["TwoSum","add","add","add","find","find"]
+    /** Find if there exists any pair of numbers which sum is equal to the value. */
+    public boolean find(int value) {
+        for (Map.Entry<Integer, Integer> entry : numCounts.entrySet()) {
+            int num = entry.getKey();
+            int complement = value - num;
+            if (complement != num) {
+                if (numCounts.containsKey(complement)) {
+                    return true;
+                }
+            } else {
+                if (entry.getValue() > 1) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum obj = new TwoSum();
+ * obj.add(number);
+ * boolean param_2 = obj.find(value);
+ */
