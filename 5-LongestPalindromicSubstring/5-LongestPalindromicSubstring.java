19b6ea81212821d09476d5ce13c969fa35ef4d04
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-18 23:34:30 -0500

    Update file 5/18/2024, 11:34:30 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,18 +1,18 @@
+public class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i); // Odd length palindromes
+            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindromes
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
     }
 
-        return s.substring(start, end + 1);
-        }
-            }
-                end = i + len / 2;
-                start = i - (len - 1) / 2;
-            if (len > end - start) {
-            int len = Math.max(len1, len2);
-            int len2 = expandAroundCenter(s, i, i + 1); // Even length palindromes
-            int len1 = expandAroundCenter(s, i, i); // Odd length palindromes
-        for (int i = 0; i < s.length(); i++) {
-        int start = 0, end = 0;
-        
-        if (s == null || s.length() < 1) return "";
-    public String longestPalindrome(String s) {
-public class Solution {
-"
+"babad"
