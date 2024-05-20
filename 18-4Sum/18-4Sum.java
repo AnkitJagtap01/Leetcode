0dd0f39b0059bb15d6a5841819a9163cb8b764f4
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-19 23:59:40 -0500

    Create file

---
+++ 18-4Sum/18-4Sum.java
@@ -0,0 +1,19 @@
+                continue; // Skip duplicate numbers
+            }
+            for (int j = i + 1; j < n - 2; j++) { // Step 2: Second number
+            return result;
+        }
+
+        Arrays.sort(nums); // Sort the array
+
+        int n = nums.length;
+        for (int i = 0; i < n - 3; i++) { // Step 2: First number
+            if (i > 0 && nums[i] == nums[i - 1]) {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 4) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue; // Skip duplicate numbers
+                }
+
+[
