476fe92b0174bbbed2c27280997965b7b6827e10
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-22 00:00:45 -0500

    Update 22-GenerateParentheses.java

--- 22-GenerateParentheses/22-GenerateParentheses.java
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -1,17 +1,21 @@
-            backtrack(result, current + ")", open, close + 1, max);
-        }
-    }
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        backtrack(result, "", 0, 0, n);
+        return result;
+    }
 
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int n1 = 3;
-        System.out.println(sol.generateParenthesis(n1));
-        // Output: ["((()))","(()())","(())()","()(())","()()()"]
+    private void backtrack(List<String> result, String current, int open, int close, int max) {
+        if (current.length() == max * 2) {
+            result.add(current);
+            return;
+        }
 
-        int n2 = 1;
-        System.out.println(sol.generateParenthesis(n2));
-        // Output: ["()"]
-    }
+        if (open < max) {
+            backtrack(result, current + "(", open + 1, close, max);
+        }
+        if (close < open) {
+            backtrack(result, current + ")", open, close + 1, max);
+        }
+    }
 }
-
-3
