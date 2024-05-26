f05f17691f59f0304db6e2f71ec1f154b381d1ff
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-25 23:57:39 -0500

    Create file

---
+++ 2235-AddTwoIntegers/2235-AddTwoIntegers.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        
+        // Test case 1
+        int num1 = 12;
+        int num2 = 5;
+        System.out.println(sol.sum(num1, num2)); // Output: 17
+        
+        // Test case 2
+        int num3 = -10;
+        int num4 = 4;
+        System.out.println(sol.sum(num3, num4)); // Output: -6
+    }
+}
+
+
+1
