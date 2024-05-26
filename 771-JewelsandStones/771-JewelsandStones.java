2bc04b82f0f0558aa207a77dc9c5112889f1fa7c
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-25 23:56:19 -0500

    Create file

---
+++ 771-JewelsandStones/771-JewelsandStones.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> jewelSet = new HashSet<>();
+        
+        // Add all jewels to the set
+        for (char j : jewels.toCharArray()) {
+            jewelSet.add(j);
+        }
+        
+        int count = 0;
+        
+        // Count how many stones are jewels
+        for (char s : stones.toCharArray()) {
+            if (jewelSet.contains(s)) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
+
+"aA"
