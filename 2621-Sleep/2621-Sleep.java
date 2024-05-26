4e8ffe6e3abb53dbe4e4854afadfc8ddd83674e3
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-25 23:59:37 -0500

    Create file

---
+++ 2621-Sleep/2621-Sleep.java
@@ -0,0 +1,18 @@
+function sleep(millis) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millis);
+  });
+}
+
+// Example usage:
+let t1 = Date.now();
+sleep(100).then(() => {
+  console.log(Date.now() - t1); // ~100
+});
+
+let t2 = Date.now();
+sleep(200).then(() => {
+  console.log(Date.now() - t2); // ~200
+});
+
+100
