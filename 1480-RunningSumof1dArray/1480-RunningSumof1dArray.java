f79c730e8109471ab57671ffae25b5845400f188
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-27 23:18:03 -0500

    Update 1480-RunningSumof1dArray.java

--- 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
+++ 1480-RunningSumof1dArray/1480-RunningSumof1dArray.java
@@ -1,14 +1,12 @@
-    }
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int[] result = new int[nums.length];
+        result[0] = nums[0];  // The first element is the same in both arrays
+
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] + nums[i];  // Each element is the sum of the previous elements
+        }
+
+        return result;
+    }
 }
-
-class Solution {
-    public int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
-        result[0] = nums[0];  // The first element is the same in both arrays
-
-        for (int i = 1; i < nums.length; i++) {
-            result[i] = result[i - 1] + nums[i];  // Each element is the sum of the previous elements
-        }
-
-        return result;
-[
