9a513c123e88f253b842ff73219913507052a22f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-05-30 23:44:48 -0500

    Update 3019-NumberofChangingKeys.java

--- 3019-NumberofChangingKeys/3019-NumberofChangingKeys.java
+++ 3019-NumberofChangingKeys/3019-NumberofChangingKeys.java
@@ -1,17 +1,15 @@
-class Solution {
-    public int countKeyChanges(String s) {
-        int keyChanges = 0;
+class Solution {
+    public int countKeyChanges(String s) {
+        int keyChanges = 0;
 
-        // Iterate through the string
-        for (int i = 1; i < s.length(); i++) {
-            // Check if the current character is different from the previous character (ignoring case)
-            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(i - 1))) {
-                keyChanges++;
-            }
-        }
+        // Iterate through the string
+        for (int i = 1; i < s.length(); i++) {
+            // Check if the current character is different from the previous character (ignoring case)
+            if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(i - 1))) {
+                keyChanges++;
+            }
+        }
 
-        return keyChanges;
-    }
+        return keyChanges;
+    }
 }
-
-"aAbBcC"
