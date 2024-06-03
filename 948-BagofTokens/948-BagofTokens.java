cbebfd1728e39bfeca815c0f1b57ce5d28bf3bb4
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-06-02 23:29:18 -0500

    Update 948-BagofTokens.java

--- 948-BagofTokens/948-BagofTokens.java
+++ 948-BagofTokens/948-BagofTokens.java
@@ -1,20 +1,31 @@
-                right--;
-                score--;
-                power += tokens[right];
-                // Play the token face-down
-            } else if (score > 0) {
-                maxScore = Math.max(maxScore, score);
-                left++;
-                score++;
-                power -= tokens[left];
-            } else {
-                // Cannot play any token
-                break;
-            }
-        }
+import java.util.Arrays;
 
-        return maxScore;
-    }
+class Solution {
+    public int bagOfTokensScore(int[] tokens, int power) {
+        Arrays.sort(tokens);
+        int score = 0;
+        int maxScore = 0;
+        int left = 0;
+        int right = tokens.length - 1;
+
+        while (left <= right) {
+            if (power >= tokens[left]) {
+                // Play the token face-up
+                power -= tokens[left];
+                score++;
+                left++;
+                maxScore = Math.max(maxScore, score);
+            } else if (score > 0) {
+                // Play the token face-down
+                power += tokens[right];
+                score--;
+                right--;
+            } else {
+                // Cannot play any token
+                break;
+            }
+        }
+
+        return maxScore;
+    }
 }
-
-[
