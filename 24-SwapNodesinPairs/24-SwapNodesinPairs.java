34e0512ef75d029cd492f4fadff5af4bee74633d
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-06-11 23:52:59 -0500

    Update 24-SwapNodesinPairs.java

--- 24-SwapNodesinPairs/24-SwapNodesinPairs.java
+++ 24-SwapNodesinPairs/24-SwapNodesinPairs.java
@@ -1,19 +1,34 @@
-class Solution {
- */
- * }
-    public ListNode swapPairs(ListNode head) {
-        // Create a dummy node to handle edge cases easily
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode current = dummy;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        // Create a dummy node to handle edge cases easily
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
 
-        while (current.next != null && current.next.next != null) {
-            // Nodes to be swapped
-            ListNode first = current.next;
-            ListNode second = current.next.next;
+        while (current.next != null && current.next.next != null) {
+            // Nodes to be swapped
+            ListNode first = current.next;
+            ListNode second = current.next.next;
 
-            // Swapping
-            first.next = second.next;
-            second.next = first;
-            current.next = second;
-[
+            // Swapping
+            first.next = second.next;
+            second.next = first;
+            current.next = second;
+
+            // Move current pointer to the next pair
+            current = first;
+        }
+
+        return dummy.next;
+    }
+}
