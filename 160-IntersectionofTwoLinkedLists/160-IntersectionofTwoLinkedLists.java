9e189d8b76e308dc8b09b45507ecf51b7bf2a316
AnkitJagtap01 <jagtap.an@northeastern.edu>
2024-11-09 19:58:08 -0500

    Update 160-IntersectionofTwoLinkedLists.java

--- 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
+++ 160-IntersectionofTwoLinkedLists/160-IntersectionofTwoLinkedLists.java
@@ -1,13 +1,26 @@
-
-        // Traverse both lists
-        while (pA != pB) {
-            // Move to the next node, or reset to the head of the other list if end is reached
-            pA = (pA != null) ? pA.next : headB;
-            pB = (pB != null) ? pB.next : headA;
-        }
-
-        // pA and pB will either meet at the intersection or at the end (null)
-        return pA;
-    }
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
 }
 
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        // Initialize two pointers for each list
+        ListNode pA = headA;
+        ListNode pB = headB;
+
+        // Traverse both lists
+        while (pA != pB) {
+            // Move to the next node, or reset to the head of the other list if end is reached
+            pA = (pA != null) ? pA.next : headB;
+            pB = (pB != null) ? pB.next : headA;
+        }
+
+        // pA and pB will either meet at the intersection or at the end (null)
+        return pA;
+    }
+}
