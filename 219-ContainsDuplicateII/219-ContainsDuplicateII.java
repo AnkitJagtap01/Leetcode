8052ad5b03db8fa6c04368a59a9e3ba1a1955e6b
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-10-07 00:34:35 -0400

    Create file

---
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.java
@@ -0,0 +1,11 @@
+                }
+                    return true;
+                if (i - prevIndex <= k) {
+                int prevIndex = map.get(nums[i]);
+            if (map.containsKey(nums[i])) {
+        
+        for (int i = 0; i < nums.length; i++) {
+       HashMap<Integer, Integer> map = new HashMap<>();
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
