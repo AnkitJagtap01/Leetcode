18de17e7e2b4576fd0cac8b0f182469e370f8685
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-10-09 00:28:33 -0400

    Create file

---
+++ 733-FloodFill/733-FloodFill.java
@@ -0,0 +1,11 @@
+    }
+
+        return image;
+        dfs(image, sr, sc, orig, color, m, n);
+        
+        if (orig == color) return image; // nothing to do
+        int orig = image[sr][sc];
+        int m = image.length, n = image[0].length;
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+class Solution {
+
