69bcdc28af4c9aa1cf656c49caf6121260b9aba5
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-10-12 23:15:18 -0400

    Update file 10/12/2025, 11:15:18 PM

--- 133-CloneGraph/133-CloneGraph.java
+++ 133-CloneGraph/133-CloneGraph.java
@@ -1,12 +1,24 @@
-            Node curr = queue.poll();
-            for (Node neighbor : curr.neighbors) {
-                if (!visited.containsKey(neighbor)) {
-                    visited.put(neighbor, new Node(neighbor.val));
-                    queue.offer(neighbor);
-                }
-                visited.get(curr).neighbors.add(visited.get(neighbor));
-            }
-        }
-        return cloneNode;
+    private Map<Node, Node> visited = new HashMap<>();
+class Solution {
+
+*/
+}
     }
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+// DFS Solution
+        // If we already cloned this node, return the clone
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        // Create a new node with the same value
+        Node cloneNode = new Node(node.val);
+        visited.put(node, cloneNode);
+
+        // Recursively clone neighbors
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
 
