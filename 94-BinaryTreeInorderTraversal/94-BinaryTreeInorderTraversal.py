de40a8453b2dccc7116fbdd837bbf75e7dac89eb
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-10-28 22:00:37 -0400

    Create file

---
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.py
@@ -0,0 +1,16 @@
+        
+        while current or stack:
+        current = root
+        result = []
+        stack = []
+class Solution:
+    def inorderTraversal(self, root: TreeNode):
+#         self.left = left
+#         self.right = right
+#         self.val = val
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+
+#Iterative approach using stack
+
