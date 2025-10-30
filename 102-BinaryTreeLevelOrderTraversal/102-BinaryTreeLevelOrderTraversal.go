8464e65395cce39bb5af4f9772cd427c0e5cc8c8
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-10-30 00:13:45 -0400

    Create file

---
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.go
@@ -0,0 +1,16 @@
+
+            if node.Left != nil {
+                queue = append(queue, node.Left)
+            }
+            if node.Right != nil {
+                queue = append(queue, node.Right)
+            }
+        }
+
+        result = append(result, level)
+    }
+
+    return result
+}
+
+
