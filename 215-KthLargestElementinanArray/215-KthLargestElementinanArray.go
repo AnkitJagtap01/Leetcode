f6975a217dadfdd5d1418db772843e2dc8a3d57e
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-11-05 00:11:41 -0500

    Update file 11/5/2025, 12:11:41 AM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.go
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.go
@@ -1,17 +1,18 @@
-//         heap.Push(h, num)
-//         if h.Len() > k {
-//             heap.Pop(h)
-//         }
-//     }
-//     return (*h)[0]
-// }
+    rand.Shuffle(n, func(i, j int) { nums[i], nums[j] = nums[j], nums[i] })
+    rand.Seed(time.Now().UnixNano())
+    // 1) Shuffle once to avoid adversarial inputs
 
+    target := n - k
+    // kth largest == (n-k)th smallest
+    n := len(nums)
 func findKthLargest(nums []int, k int) int {
-    target := len(nums) - k // kth largest == (n-k)th smallest
-    l, r := 0, len(nums)-1
-    for {
-        p := partition(nums, l, r)
-        if p == target {
+
 // Quickselect
 
+// }
+
+    l, r := 0, n-1
+    for l <= r {
+        lt, gt := partition3(nums, l, r) // nums[l:lt] < p, nums[lt:gt+1] == p, nums[gt+1:r+1] > p
+        if target < lt {
 
