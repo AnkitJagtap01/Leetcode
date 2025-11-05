8115396f4506a334df3902c332479074a3421c7f
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-11-05 00:08:34 -0500

    Update file 11/5/2025, 12:08:34 AM

--- 215-KthLargestElementinanArray/215-KthLargestElementinanArray.go
+++ 215-KthLargestElementinanArray/215-KthLargestElementinanArray.go
@@ -1,16 +1,17 @@
-type IntHeap []int
-    h := &IntHeap{}
-    heap.Init(h)
-
-    for _, num := range nums {
+//         heap.Push(h, num)
+//         if h.Len() > k {
+//             heap.Pop(h)
+//         }
+//     }
+//     return (*h)[0]
+// }
 
 func findKthLargest(nums []int, k int) int {
+    target := len(nums) - k // kth largest == (n-k)th smallest
+    l, r := 0, len(nums)-1
+    for {
+        p := partition(nums, l, r)
+        if p == target {
+// Quickselect
 
-func (h IntHeap) Len() int            { return len(h) }
-func (h IntHeap) Less(i, j int) bool  { return h[i] < h[j] } // min-heap
-func (h IntHeap) Swap(i, j int)       { h[i], h[j] = h[j], h[i] }
-func (h *IntHeap) Push(x any)         { *h = append(*h, x.(int)) }
-func (h *IntHeap) Pop() any           { old := *h; x := old[len(old)-1]; *h = old[:len(old)-1]; return 
-x }
-        heap.Push(h, num)
 
