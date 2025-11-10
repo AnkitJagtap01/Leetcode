ab21d2f3160f9dda0e75a5a1d0d29ecdbc65342c
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-11-09 23:50:03 -0500

    Update file 11/9/2025, 11:50:03 PM

--- 322-CoinChange/322-CoinChange.go
+++ 322-CoinChange/322-CoinChange.go
@@ -1,14 +1,16 @@
+func coinChange(coins []int, amount int) int {
+    if amount == 0 {
+        return 0
+    }
+
+    dp := make([]int, amount+1)
+    for i := 1; i <= amount; i++ {
+        dp[i] = math.MaxInt32
+    }
+
     for _, coin := range coins {
         for i := coin; i <= amount; i++ {
             if dp[i-coin] != math.MaxInt32 {
                 dp[i] = min(dp[i], dp[i-coin]+1)
             }
-        }
-    }
-
-    if dp[amount] == math.MaxInt32 {
-        return -1
-    }
-    return dp[amount]
-}
 
