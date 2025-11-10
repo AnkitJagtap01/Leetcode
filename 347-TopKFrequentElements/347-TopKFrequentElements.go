7178c4976660b1a615ca05e06703a2c8ab656067
AnkitJagtap01 <jagtap.an@northeastern.edu>
2025-11-10 10:13:10 -0500

    Create file

---
+++ 347-TopKFrequentElements/347-TopKFrequentElements.go
@@ -0,0 +1,16 @@
+        buckets[f] = append(buckets[f], val)
+    for val, f := range freq {
+    buckets := make([][]int, maxF+1)
+    // 2) buckets: index = frequency, value = list of numbers with that freq
+
+    }
+        }
+            maxF = freq[x]
+        if freq[x] > maxF {
+    for _, x := range nums {
+        freq[x]++
+    maxF := 0
+    freq := make(map[int]int)
+    // 1) frequency map
+func topKFrequent(nums []int, k int) []int {
+
